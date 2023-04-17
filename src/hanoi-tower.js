4356b85b18b9ec358bea5bda0cf1e45bf6eb21cb
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-04-17 18:42:00 +0300

    feat: hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+let countH = 0;
+
+function calculateHanoi(disksNumber, turnsSpeed) {
+	// let countH = 0;
+
+	function toh(n) {
+		countH++;
+		if (n > 1) {
+			toh(n - 1);
+			// console.log("Move disk from tower ",src,"to tower",des);
+			toh(n - 1);
+		}
+		return;
+	}
+
+	toh(disksNumber);
+	const countTime = Math.round(countH / ((turnsSpeed / 60) / 60));
+	return {
+		turns: countH,
+		seconds: countTime,
+	};
+	// throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
 }
 
+// console.log(calculateHanoi(9, 4308));
 module.exports = {
-  calculateHanoi
+	calculateHanoi
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+	// console.log(Date.parse(date));
+	// if (isNaN(Date.parse(date))) return 'Unable to determine the time of year!';
+	// const month = date.getMonth();
+	// const day = date.getDate();
+	// console.log(day);
+	// console.log(month)
+	// let season;
+	// if (month >= 1 && month <= 3) season = "winter";
+	// if (month === 3 && day > 19) season = "spring";
+	// if (month > 3 && month <= 6) season = "spring";
+	// if (month === 6 && day > 20) season = "summer";
+	// if (month > 6 && month <= 9) season = "summer";
+	// if (month === 9 && day > 21) season = "fall";
+	// if (month > 9 && month <= 12) season = "fall";
+	// if (month === 12 && day > 20) season = "winter";
+	// return season;
+//Y2K Fix
+	throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
 }
 
+// const fakeDate = {
+// 	toString() {
+// 		return Date.prototype.toString.call(new Date());
+// 	},
+// 	[Symbol.toStringTag]: 'Date'
+// };
+// console.log(getSeason(fakeDate));
 module.exports = {
-  getSeason
+	getSeason
 };
